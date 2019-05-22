7c518f2682fe244c89a3bf9799a0bc607ee71025
leonardofernandescosta <leonardo.fc2@outllok.com>
2019-05-22 15:07:59 -0300

    Update main.js

--- ConsultaCep/src/pages/main.js
+++ ConsultaCep/src/pages/main.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View,Text,FlatList,TouchableOpacity,StyleSheet} from 'react-native'
+import {View,Text,FlatList,TouchableOpacity,StyleSheet,TextInput} from 'react-native'
 import api from '../services/api'
 
 export default class Main extends Component
@@ -9,13 +9,58 @@ export default class Main extends Component
     };
 
 
+    state=
+    {
+        docs:[],
+        cepInput:""
+    };
+
+    ValidaCep (cepRecebido)
+    {
+        return
+        {
+            console.log("valida cep")
+            console.log("Cep recebido: " +cepRecebido); 
+        }
+    }
+
     render()
     {
         return(
             <View>
             <Text>Consulta CEP</Text>
             <Text>Busque endereços de forma simples digitando apenas o  CEP</Text>
+            <TextInput
+                onChangeText={(cepInput)=>this.setState({cepInput})}
+                value={this.state.cepInput}
+                placeholder="242200-045">
+            </TextInput>
+            <TouchableOpacity
+                style={styles.botaoConfirmar}
+                onPress={this.ValidaCep}>
+                <Text>
+                    Confirmar
+                </Text>
+                
+            </TouchableOpacity>
             </View>
         );
     }
 }
+
+
+/* CSS*/
+
+const styles=StyleSheet.create({
+        inputDoCep:
+        {
+            fontSize:26,
+            color:"black",
+            fontWeight:"bold",
+            lineHeight: 10
+        },
+        botaoConfirmar:
+        {
+            fontSize:20
+        }
+    });
